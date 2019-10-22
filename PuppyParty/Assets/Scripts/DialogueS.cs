2e75b8b0c596a8e83b4c6495d54acd3663ef3997
Unknown <Tori7809@gmail.com>
2019-10-22 13:09:13 -0600

    Scene Changer, button

---
+++ PuppyParty/Assets/Scripts/DialogueS.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+
+public class DialogueS : MonoBehaviour
+{
+    public string text;
+
+    public UnityEvent EndOfTextEvent;
+
+   // public Panel panel;
+    
+    public Text DialogueBox;
+    
+    //Call when DialogueBox is opened
+    
+    public void DisplayText()
+    {
+       // DialogueBoxText = text
+    }
+    
+    // call when next button is pressed
+    
+    public void CloseBox()
+    {
+        
+    }
+    
+    
+}
