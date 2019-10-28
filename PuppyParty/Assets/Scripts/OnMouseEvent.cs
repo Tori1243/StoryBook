6d6eed2e7d689eb821e85133fb362efed0293af3
Unknown <Tori7809@gmail.com>
2019-10-28 13:58:56 -0600

    Particle System

---
+++ PuppyParty/Assets/Scripts/OnMouseEvent.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+
+public class OnMouseEvent : MonoBehaviour
+{
+   public UnityEvent OnMouseDownEvent;
+   
+   private void OnMouseDown()
+    {
+        OnMouseDownEvent.Invoke();
+    }
+}
