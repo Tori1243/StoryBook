0f94c7812664d8f804f14cca5c8e97d230b2c883
Unknown <Tori7809@gmail.com>
2019-10-30 11:02:43 -0600

    Draggable Objects

---
+++ PuppyParty/Assets/Scripts/TriggerEventBehavior.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+public class TriggerEventBehavior : MonoBehaviour
+{
+    public UnityEvent triggerEnterEvent, triggerExitEvent;
+    private void OnTriggerEnter(Collider other)
+    {
+        triggerEnterEvent.Invoke();
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        triggerExitEvent.Invoke();
+    }
+}
