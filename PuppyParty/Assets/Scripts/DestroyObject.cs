f05364dc28baa68fbdc73dfd7cfbb8f9c9c2d4b8
Unknown <Tori7809@gmail.com>
2019-11-30 14:30:29 -0700

    DestroyObject script Trails

--- PuppyParty/Assets/Scripts/DestroyObject.cs
+++ PuppyParty/Assets/Scripts/DestroyObject.cs
@@ -1,14 +1,16 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
 using UnityEngine.Events;
+using UnityEngine.EventSystems;
 
 public class DestroyObject : MonoBehaviour
 {
     public UnityEvent ClickEvent;
-    
+
     private void OnMouseDown()
     {
         ClickEvent.Invoke();
         Destroy(gameObject);
     }
-    
+
 }
