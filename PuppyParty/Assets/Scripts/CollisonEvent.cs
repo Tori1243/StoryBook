4361aba534f4a60d1fc3a6a6764d2617c31bfed0
Unknown <Tori7809@gmail.com>
2019-12-04 13:29:08 -0700

    Collision and trigger text

---
+++ PuppyParty/Assets/Scripts/CollisonEvent.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class CollisonEvent : MonoBehaviour
+{
+
+	public UnityEvent CollisionEnterEvent;
+
+	private void OnCollisionEnter(Collision other)
+	{
+		CollisionEnterEvent.Invoke();
+	}
+
+}
